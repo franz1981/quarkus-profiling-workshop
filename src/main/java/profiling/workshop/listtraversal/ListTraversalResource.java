9452cd57435fda89fe50191d2eb540e0660b4a9d
franz1981 <nigro.fra@gmail.com>
2023-01-02 18:17:59 +0100

    Simplifying traversal case

--- src/main/java/profiling/workshop/listtraversal/ListTraversalResource.java
+++ src/main/java/profiling/workshop/listtraversal/ListTraversalResource.java
@@ -1,26 +1,23 @@
 package profiling.workshop.listtraversal;
 
-import java.time.ZoneId;
-import java.time.format.TextStyle;
-import java.util.Locale;
+import io.smallrye.common.annotation.NonBlocking;
+
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import io.quarkus.runtime.annotations.RegisterForReflection;
-import profiling.workshop.time.TimeService;
-
-@Path("/list")
+@Path("/sum")
 public class ListTraversalResource {
 
     @Inject
     ListTraversalService service;
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public long processList() {
-        return service.getResultWithTraversal();
+    @Produces(MediaType.TEXT_PLAIN)
+    @NonBlocking
+    public long sum() {
+        return service.sum();
     }
 }
--- src/main/java/profiling/workshop/listtraversal/ListTraversalService.java
+++ src/main/java/profiling/workshop/listtraversal/ListTraversalService.java
@@ -1,49 +1,28 @@
 package profiling.workshop.listtraversal;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import javax.annotation.PostConstruct;
 import javax.inject.Singleton;
 
 @Singleton
 public class ListTraversalService {
+    private List<Integer> myList;
 
-    private static final List<Integer> MY_LIST = new ArrayList<>();
-
-    static {
-        for (int i = 0; i < 10_000_000; i++) {
-            MY_LIST.add(i);
+    @PostConstruct
+    public void initList() {
+        myList = new LinkedList<>();
+        for (int i = 0; i < 1024; i++) {
+            myList.add(ThreadLocalRandom.current().nextInt(0, Integer.MAX_VALUE));
         }
     }
 
-    public long getResultWithPositionalAccess() {
-        int result = 0;
-        for (int i = 0; i < MY_LIST.size(); i++) {
-            if (i % 16 == 0) {
-                result += MY_LIST.get(i);
-            }
+    public long sum() {
+        int sum = 0;
+        for (int i = 0; i < myList.size(); i++) {
+            sum += myList.get(i);
         }
-        return result;
-    }
-
-    public long getResultWithTraversal() {
-        long result = 0;
-        int i = 0;
-        for (int item : MY_LIST) {
-//            if (i % 16 == 0) {
-            if ((i & 15) == 0) {
-                result += item;
-            }
-            i++;
-        }
-        return result;
-    }
-
-    public static void main(String[] args) {
-        ListTraversalService service = new ListTraversalService();
-        long start = System.nanoTime();
-        long result = service.getResultWithTraversal();
-        long duration = (System.nanoTime() - start) / 1_000_000;
-        System.out.println(result + " calculated in " + duration + " msecs");
+        return sum;
     }
 }
