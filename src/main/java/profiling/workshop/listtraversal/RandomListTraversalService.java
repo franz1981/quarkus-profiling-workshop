f290866067b20113d9d5e66439316fa7da01899a
mariofusco <mario.fusco@gmail.com>
2023-01-03 13:06:11 +0100

    add cache misses examples

---
+++ src/main/java/profiling/workshop/listtraversal/RandomListTraversalService.java
@@ -0,0 +1,50 @@
+package profiling.workshop.listtraversal;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.PostConstruct;
+import javax.inject.Singleton;
+
+@Singleton
+public class RandomListTraversalService {
+
+    private static int LIST_SIZE = 1_000_000;
+
+    private static Collection<Integer> indexes = generateIndexes(false);
+
+    private static List<Integer> values = new ArrayList<>(LIST_SIZE);
+
+    @PostConstruct
+    public void initLists() {
+        for (int i = 0; i < LIST_SIZE; i++) {
+            values.add(i);
+        }
+    }
+
+    private static Collection<Integer> generateIndexes(boolean sequential) {
+        if (sequential) {
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < LIST_SIZE; i++) {
+                list.add(i);
+            }
+            return list;
+        } else {
+            Map<String, Integer> map = new HashMap<>();
+            for (int i = 0; i < LIST_SIZE; i++) {
+                map.put("" + i, i);
+            }
+            return map.values();
+        }
+    }
+
+    public long getResultWithTraversal() {
+        long result = 0;
+        for (int index : indexes) {
+            result += values.get(index);
+        }
+        return result;
+    }
+}
