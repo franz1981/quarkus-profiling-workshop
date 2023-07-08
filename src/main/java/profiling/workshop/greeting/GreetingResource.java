a1cca2d07deefebb0b56c60782b24992ab965021
Francesco Nigro <nigro.fra@gmail.com>
2023-07-08 10:26:09 +0200

    Remove NonBlocking

--- src/main/java/profiling/workshop/greeting/GreetingResource.java
+++ src/main/java/profiling/workshop/greeting/GreetingResource.java
@@ -1,7 +1,5 @@
 package profiling.workshop.greeting;
 
-import io.smallrye.common.annotation.NonBlocking;
-
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -12,7 +10,6 @@ public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    @NonBlocking
     public String hello() {
         return "Hello from RESTEasy Reactive";
     }
