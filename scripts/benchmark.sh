c64e219538d6ad2a165f31c34a3b1e7eba150470
Francesco Nigro <nigro.fra@gmail.com>
2024-03-21 10:46:09 +0100

    Switch to h2load to have http 2 support. Add new aploader version Add results & profiling data

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -13,17 +13,47 @@ FORMAT=html
 
 JFR=false
 
-THREADS=2
+THREADS=1
 
 RATE=0
 
-CONNECTIONS=10
+CONNECTIONS=20
+
+PIPELINING=1
 
 JFR_ARGS=
 
 PERF=false
 
-WRK_PROFILING=false
+# partially disable inflation: move to JDK 17 and use -Dsun.reflect.inflationThreshold=2147483647
+
+QUARKUS_JVM_ARGS="-Dquarkus.vertx.prefer-native-transport=true"
+
+OPT_QUARKUS_JVM_ARGS="-Dquarkus.http.limits.max-body-size= \
+                   -Dquarkus.vertx.prefer-native-transport=true  \
+                   -XX:-StackTraceInThrowable \
+                   -Dquarkus.http.accept-backlog=-1 \
+                   -Dio.netty.buffer.checkBounds=false \
+                   -Dio.netty.buffer.checkAccessible=false \
+                   -Djava.util.logging.manager=org.jboss.logmanager.LogManager \
+                   -Dquarkus.http.idle-timeout=0 \
+                   -XX:+UseNUMA \
+                   -XX:+UseParallelGC \
+                   -Djava.lang.Integer.IntegerCache.high=10000 \
+                   -Dvertx.disableURIValidation=true \
+                   -Dvertx.disableHttpHeadersValidation=true \
+                   -Dvertx.disableMetrics=true \
+                   -Dvertx.disableH2c=true \
+                   -Dvertx.disableWebsockets=true \
+                   -Dvertx.flashPolicyHandler=false \
+                   -Dvertx.threadChecks=false \
+                   -Dvertx.disableContextTimings=true \
+                   -Dhibernate.allow_update_outside_transaction=true \
+                   -Dio.quarkus.vertx.core.runtime.context.VertxContextSafetyToggle.I_HAVE_CHECKED_EVERYTHING=true \
+                   -Djboss.threads.eqe.statistics=false \
+                   -Dmutiny.disableCallBackDecorators=true"
+
+HTTP_2=false
 
 Help()
 {
@@ -61,7 +91,7 @@ Help()
    echo "p    if specified, run perf stat together with the selected profiler. Only GNU Linux."
 }
 
-while getopts "hu::e::f::d::jt::r::c:p" option; do
+while getopts "hu::e::f::d::jt::r::c::m:p" option; do
    case $option in
       h) Help
          exit;;
@@ -80,6 +110,8 @@ while getopts "hu::e::f::d::jt::r::c:p" option; do
       r) RATE=${OPTARG}
          ;;
       c) CONNECTIONS=${OPTARG}
+         ;;
+      m) PIPELINING=${OPTARG}
          ;;
       p) PERF=true
          ;;
@@ -111,7 +143,7 @@ trap 'echo "cleaning up quarkus process";kill ${quarkus_pid}' SIGINT SIGTERM SIG
 
 # let's run it with a single thread, is simpler!
 # TODO cmd can be extracted and become a run-quarkus.sh script per-se
-java ${JFR_ARGS} -Dquarkus.vertx.event-loops-pool-size=${THREADS} -XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints -jar ../target/quarkus-app/quarkus-run.jar &
+java ${JFR_ARGS} -Dquarkus.vertx.event-loops-pool-size=${THREADS} ${QUARKUS_JVM_ARGS} -jar ../target/quarkus-app/quarkus-run.jar &
 quarkus_pid=$!
 
 sleep 2
@@ -121,13 +153,23 @@ echo "----- Quarkus running at pid $quarkus_pid using ${THREADS} I/O threads"
 if [ "${RATE}" != "0" ]
 then
   echo "----- Start fixed rate test at ${RATE} requests/sec and profiling"
-  ${HYPERFOIL_HOME}/bin/wrk2.sh -R ${RATE} -c ${CONNECTIONS} -t ${THREADS} -d ${DURATION}s ${FULL_URL} &
+  if [ "${HTTP_2}" = true ]
+  then
+    h2load ${FULL_URL} -c ${CONNECTIONS} -D ${DURATION}s -t ${THREADS} -m ${PIPELINING} -r {RATE} &
+  else
+    h2load ${FULL_URL} -c ${CONNECTIONS} -D ${DURATION}s -t ${THREADS} -m ${PIPELINING} -r {RATE} --h1 &
+  fi
 else
   echo "----- Start all-out test and profiling"
-  ${HYPERFOIL_HOME}/bin/wrk.sh -c ${CONNECTIONS} -t ${THREADS} -d ${DURATION}s ${FULL_URL} &
+  if [ "${HTTP_2}" = true ]
+    then
+      h2load ${FULL_URL} -c ${CONNECTIONS} -D ${DURATION}s -t ${THREADS} -m ${PIPELINING} &
+    else
+      h2load ${FULL_URL} -c ${CONNECTIONS} -D ${DURATION}s -t ${THREADS} -m ${PIPELINING} --h1 &
+    fi
 fi
 
-wrk_pid=$!
+load_generation_pid=$!
 
 echo "----- Waiting $WARMUP seconds before profiling for $PROFILING seconds"
 
@@ -139,14 +181,8 @@ if [ "${JFR}" = true ]
 then
   jcmd $quarkus_pid JFR.start duration=${PROFILING}s filename=${NOW}.jfr dumponexit=true settings=profile
 else
-  if [ "${WRK_PROFILING}" = true ]; then
-     JFR_ARGS=-XX:+FlightRecorder
-     wrk_jvm_pid=`jps | grep Wrk | awk '{print $1}'`
-     echo "----- Starting async-profiler on load generator process ($wrk_jvm_pid)"
-     java -jar ap-loader-all.jar profiler -e ${EVENT} -t -d ${PROFILING} -f wrk_${NOW}_${EVENT}.${FORMAT} $wrk_jvm_pid &
-  fi
   echo "----- Starting async-profiler on quarkus application ($quarkus_pid)"
-  java -jar ap-loader-all.jar profiler -e ${EVENT} -t -d ${PROFILING} -f ${NOW}_${EVENT}.${FORMAT} $quarkus_pid &
+  java -jar ap-loader-all.jar profiler -e ${EVENT} -d ${PROFILING} -t -f ${NOW}_${EVENT}.${FORMAT} $quarkus_pid &
 fi
 
 ap_pid=$!
@@ -180,7 +216,7 @@ if [ "${PERF}" = true ]; then
   kill -SIGINT $stat_pid
 fi
 
-wait $wrk_pid
+wait $load_generation_pid
 
 echo "----- Profiling and workload completed: killing server"
 
