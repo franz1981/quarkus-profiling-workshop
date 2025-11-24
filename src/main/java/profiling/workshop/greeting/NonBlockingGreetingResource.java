7198a4bc9a2d528f6a267738a9aa836c8ea6164e
Francesco Nigro <nigro.fra@gmail.com>
2025-11-24 05:31:47 +0100

    Add non blocking endpoint

---
+++ src/main/java/profiling/workshop/greeting/NonBlockingGreetingResource.java
@@ -0,0 +1,18 @@
+package profiling.workshop.greeting;
+
+import io.smallrye.common.annotation.NonBlocking;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/async-hello")
+public class NonBlockingGreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @NonBlocking
+    public String hello() {
+        return "Hello from RESTEasy Reactive";
+    }
+}
